1c8f514a0e86feedbaa2011f776df8bfd7200951
MSD-12 <magandra1999@gmail.com>
2024-05-15 00:33:21 +0530

    Striver Playlist

---
+++ Walkie_Talkie/Pattern/src/Second.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Second {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Enter any Character");
+        String symbol = sc.next();
+
+        for (int i = 0; i < 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+                for (int j = 0; j <= i; j++)
+                    System.out.print(symbol);
+            System.out.println();
+        }
+    }
+
+}
